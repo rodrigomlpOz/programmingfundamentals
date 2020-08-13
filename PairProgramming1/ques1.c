6457c5204033de2319739626ed9a6d9d556324ea
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2020-08-13 17:10:46 -0300

    Update ques1.c

--- PairProgramming1/ques1.c
+++ PairProgramming1/ques1.c
@@ -16,10 +16,14 @@ int main()
 {
    int i,j,rows;
    rows = get_int("Input no. of rows:\n");
+   //controls rows
    for(i=1;i<=rows;i++)
    {
-	for(j=1;j<=i;j++)
-	   printf("*");
-	   printf("\n");
+	//control columns
+   	for(j=1;j<=i;j++)
+   	{
+   	  printf("*");
+   	}
+	printf("\n");
    }
 }
