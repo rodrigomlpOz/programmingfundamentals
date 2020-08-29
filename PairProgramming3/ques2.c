9eda602c2e69d2ec4cb0f1b579d5046248a9fb96
Deepa Prasanna <anneraj73@gmail.com>
2020-08-29 15:18:52 +0530

    Update ques2.c

--- PairProgramming3/ques2.c
+++ PairProgramming3/ques2.c
@@ -1,5 +1,6 @@
 /* Write a program in C to read n number of values
-   in an array and display it in reverse order.
+   in an array and display it in reverse order. The program should prompt
+   the user to enter the no. of elements and store it in a variable named n.
 
 Test Input Data :
 
@@ -30,18 +31,18 @@ int main()
    for (i = 0; i < n; i++)
    {
 	   a[i] = get_int("element - %d : ", i);
-	}
+   }
 
    printf("\nThe values stored into the array are : \n");
    for(i = 0; i < n; i++)
    {
 	   printf("% 5d", a[i]);
-	}
+   }
 
    printf("\n\nThe values stored into the array in reverse are :\n");
    for(i = n-1; i >= 0;i--)
    {
 	   printf("% 5d", a[i]);
-	}
+   }
    printf("\n\n");
 }
