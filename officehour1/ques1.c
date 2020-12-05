39e8cfa347e6d7db3bbf4e3f8c882ad1c6b4b54e
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2020-12-04 22:31:10 -0300

    Update ques1.c

--- officehour1/ques1.c
+++ officehour1/ques1.c
@@ -1,13 +1,13 @@
-/* Write a C program to find the sum of first 10 natural numbers. */
+/* Write a C program to find the sum of the number from 1 to 10 using for loops. */
 
 #include <stdio.h>
 void main()
 {
-    int  j, sum = 0;
+    int sum = 0;
 
     printf("The first 10 natural number is :\n");
  
-    for (j = 1; j <= 10; j++)
+    for (int j = 1; j <= 10; j++)
     {
         sum = sum + j;
         printf("%d ",j);    
