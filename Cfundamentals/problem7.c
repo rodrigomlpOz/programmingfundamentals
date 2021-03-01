a6967b96002b17d062c2347337de7b945a225202
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-03-01 19:44:58 -0300

    Update problem7.c

--- Cfundamentals/problem7.c
+++ Cfundamentals/problem7.c
@@ -1,4 +1,4 @@
-/* Write a C program to check which number nearest to the value 100 among two
+/* Write a C program to check which number is closest to the value 100 among two
 given integers. Return 0 if the two numbers are equal. */
 
 #include <stdio.h>
