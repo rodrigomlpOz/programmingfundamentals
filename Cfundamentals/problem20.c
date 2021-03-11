1dfbb7761b33c25047faa860a9cce810e144e1b1
Rodrigo <rodrigomlp@gmail.com>
2021-03-11 14:29:18 -0300

    nproblem20

---
+++ Cfundamentals/problem20.c
@@ -0,0 +1,11 @@
+//Grab the first 2 digits of 23032234
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num = 23032234;
+    int first_two_digits1 = 23032234 / 1000000;
+    int first_two_digits2 = 23032234 / pow(10, 6);
+}
+
