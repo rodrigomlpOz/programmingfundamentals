130f6df083115aa8f0f8259e073d8d112afb92d9
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-03-18 17:15:21 -0300

    Update problem20.c

--- Cfundamentals/problem20.c
+++ Cfundamentals/problem20.c
@@ -1,11 +1,15 @@
-//Grab the first 2 digits of 23032234
+//Grab the first 2 digits of 23032234 and print it
 #include <stdio.h>
 #include <math.h>
 
-int main()
+int main(void)
 {
     int num = 23032234;
+    //Option 1
     int first_two_digits1 = 23032234 / 1000000;
+    printf("%d\n", first_two_digits1);
+    //Better option
     int first_two_digits2 = 23032234 / pow(10, 6);
+    printf("%d\n", first_two_digits2);
 }
 
