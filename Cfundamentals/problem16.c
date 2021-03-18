c6549e0c93d449be6b90aa0eb42ac8363f9ae51f
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-03-18 16:51:48 -0300

    Update problem16.c

--- Cfundamentals/problem16.c
+++ Cfundamentals/problem16.c
@@ -1,22 +1,30 @@
-/* Write a program in C to make such a pattern like right angle triangle with
-  a number which will repeat a number in a row. 
-  
-The pattern like :
+/* 
 
- 1
- 22
- 333
- 4444  
+  Write a program in C to make such a pattern like the triangle below
+  Ask the user for the number of rows
+  At each row, you will print the row that you are currently at
   
-  */
+
+  num_rows = 4
+   
+  1
+  22
+  333
+  4444  
+ 
+  num_rows = 2
+  1
+  22
+  
+ */
 
 #include <stdio.h>
 #include <cs50.h>
 
-int main()
+int main(void)
 {
    int rows = get_int("Input number of rows : ");
-   for(int i=1;i<=rows;i++)
+   for(int i = 1; i <= rows; i++)
    {
 	   for(int j=1;j<=i;j++)
       {
