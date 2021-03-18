450e0ac3bb67a041b937b92d3e94b98f250ce6a1
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-03-18 15:32:48 -0300

    Update problem2.c

--- Cfundamentals/problem2.c
+++ Cfundamentals/problem2.c
@@ -1,7 +1,14 @@
-//Print "cough" in 3 ways
-//First using 3 printf statements
-// Then using a loop
-// then using a function
+/*
+  Print "cough" 3 times in 3 ways. The output should look like
+      
+  1) First using 3 printf statements
+  2) Then using a loop
+  3) By calling a function
+  
+  You should see 9 "coughs" on the terminal at the end of the problem
+*/
+
+
 
 #include <stdio.h>
 void cough(int x);
