16e2f48902fb07bbf7347b5257553f6ab6c080a5
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-03-18 15:43:47 -0300

    Update problem8.c

--- Cfundamentals/problem8.c
+++ Cfundamentals/problem8.c
@@ -1,6 +1,14 @@
-//Ask user for integer input which will be our "change"
-//Use a while loop to determine how many quarters
-//will be given as "change"
+/*
+   Ask user for an integer input which will be our stored in a variable called "change"
+   Use a while loop to determine how many quarters will be given as "change"
+   
+   Example:
+   
+   If the change is 72, we return 3 quarters
+   If the change is 25 we return 2 quarters
+   If the change is 32 we return 2 quarters
+   If the change is 11 we return 0 quarters
+*/
 
 #include <stdio.h>
 #include <cs50.h>
@@ -9,7 +17,7 @@ int main(void)
 {
     int x = get_int("What's your change?\n");
     int count_of_quarters = 0;
-    while (x > 25)
+    while (x >= 25)
     {
         x -= 25;
         count_of_quarters += 1;
