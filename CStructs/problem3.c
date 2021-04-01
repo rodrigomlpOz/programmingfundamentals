227fea04f536ac0f7f43ba1819d2b8107264424b
Rodrigo <rodrigomlp@gmail.com>
2021-04-01 03:06:31 -0300

    add

--- CStructs/problem3.c
+++ CStructs/problem3.c
@@ -13,8 +13,8 @@ int main()
     {
       if (array[j] > array[j+1]) /* For decreasing order use '<' instead of '>' */
       {
-        int swap       = array[j];
-        array[j]   = array[j+1];
+        int swap = array[j];
+        array[j] = array[j+1];
         array[j+1] = swap;
       }
     }
